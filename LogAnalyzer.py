7ce54d18be39a24ee1638c287ba0bd96d4e029a7
Dhatri246 <dhatrimanupati2024@gmail.com>
2026-09-10 21:14:28 +0530

    feat: add initial logic to read and display log file content

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -1,1 +1,13 @@
- 
+import os
+from pathlib import Path
+
+file = input("Enter a valid file path: ")
+
+if os.path.exists(file):
+    text = open(file).read()
+    print(text)
+
+else:
+    print("File does not exist!") 
+
+    
