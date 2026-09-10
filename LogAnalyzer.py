43080fdc99cf1c954f51128d62cb03fcf700c902
Dhatri246 <dhatrimanupati2024@gmail.com>
2026-09-10 22:27:25 +0530

    feat: added main and wrapped the code in functions

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -1,13 +1,19 @@
 import os
 from pathlib import Path
 
-file = input("Enter a valid file path: ")
+def checkPath(file_path):
+    if os.path.exists(file_path):
+        readFile(file_path)
+    else:
+        print("File does not exist!")   
 
-if os.path.exists(file):
-    text = open(file).read()
-    print(text)
 
-else:
-    print("File does not exist!") 
+def readFile(file_path):
 
+    file = open(file_path).read()
+    print(file)
     
+
+def main():
+    file_path = input("Enter a valid file path: ")
+    checkPath(file_path)
